0e49108953da7b8d8a6e9d2790a7feb4ca1e327c
Huiying Shen <hshen@ski.org>
2024-05-13 13:18:39 -0700

    Initial Commit

---
+++ CamIo Hands Cpp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CamIo Hands Cpp
+//
+//  Created by Huiying Shen on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
